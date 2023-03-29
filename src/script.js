d9e5aefbc0d34646d3cf51d9fb789ea737bd3d87
aleganza <ganzarolli.alessio@gmail.com>
2023-03-30 00:41:11 +0200

    add main page style, search bar linked to function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+/* press enter on input */
+var input = document.getElementById("search-input");
+var button = document.getElementById("search-button");
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    button.click();
+  }
+});
+
+
+
 async function fetchAPI() {
     var url = "https://my-burger-api.herokuapp.com/burgers";
 
@@ -6,7 +19,28 @@ async function fetchAPI() {
 
     console.log(jsonResponse);
 
+    
+
     for (var i=0; i<28; i++) {
-        var section = document.getElementById("id").innerHTML += jsonResponse[i].name + " - ";
+        document.getElementById("items").innerHTML += jsonResponse[i].name + " - ";
     }
 }
+
+/* function searchContact(){
+    var a, i, txtValue;
+    var input = document.getElementById('searchedId');
+    var filter = input.value.toUpperCase();
+    var list = document.getElementById("list");
+    var contact = list.getElementsByClassName('bg');
+
+    // scorre la lista dei contatti e mostra solo quelli che contengono lettere cercate
+    for (i = 0; i < contact.length; i++) {
+        a = contact[i].getElementsByClassName("contact")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            contact[i].style.display = "";
+        } else {
+            contact[i].style.display = "none";
+        }
+    }
+} */
